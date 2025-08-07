4392723e914175e1e40f8489175e5afc6235617e
Gona Akshay Kumar <akshaygona83@gmail.com>
2025-08-07 17:18:30 +0530

    Update api.js
    hardcoded onrender api backend url

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000';
+const baseUrl = 'https://kingdom-olj1.onrender.com';
 
 export async function uploadSwaggerFile(file) {
   const formData = new FormData();
